c003dcc2fd927395f508506e54afbd94e592b8f3
tom_ptrs@yahoo.com <tom_ptrs@yahoo.com>
2018-09-06 13:27:40 +0200

    unity files

---
+++ Assets/ShowAllButThis.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ShowAllButThis : MonoBehaviour, ITrackableEventHandler
+{
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+       
+    }
+
+    // Use this for initialization
+    private TrackableBehaviour mTrackableBehaviour;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
